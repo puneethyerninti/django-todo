ab7e511d346d1ef25d1c612e63b6cbafdc2fe23d
puneethyerninti <146336674+puneethyerninti@users.noreply.github.com>
2025-06-24 20:45:13 +0530

    Temp superuser creation

--- todoprj/settings.py
+++ todoprj/settings.py
@@ -126,3 +126,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
 
+# Temporary admin auto-creation (remove after first login)
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "adminpass")
