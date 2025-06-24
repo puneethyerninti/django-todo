04c9c975bd4a8c7ce98171d49df442cdb45f5c1e
puneethyerninti <146336674+puneethyerninti@users.noreply.github.com>
2025-06-24 20:50:30 +0530

    Added CSRF trusted origin for Railway domain

--- todoprj/settings.py
+++ todoprj/settings.py
@@ -132,3 +132,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username="admin").exists():
     User.objects.create_superuser("admin", "admin@example.com", "adminpass")
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-todo-final-production.up.railway.app'
+]
